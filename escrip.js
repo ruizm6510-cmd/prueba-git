cb0beaffe7d030c8f7148ad3f8a16df9da316916
manuel <ruizm6510@gmail.com>
2025-09-23 20:14:50 -0500

    esto es lo de javascrip

--- escrip.js
+++ escrip.js
@@ -1,11 +1,26 @@
-import React from 'react'
+function calcular() {
+  let num1 = parseFloat(document.getElementById("num1").value);
+  let num2 = parseFloat(document.getElementById("num2").value);
+  let op = document.getElementById("op").value;
 
-const escrip = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+  let resultado = 0;
+
+  if (op == "+") {
+    resultado = num1 + num2;
+  }
+  if (op == "-") {
+    resultado = num1 - num2;
+  }
+  if (op == "*") {
+    resultado = num1 * num2;
+  }
+  if (op == "/") {
+    if (num2 == 0) {
+      resultado = "Error: división entre 0";
+    } else {
+      resultado = num1 / num2;
+    }
+  }
+
+  document.getElementById("resultado").innerText = "Resultado: " + resultado;
 }
-
-export default escrip
